156dba7f883aedf10aa6d8391fc49bf59a3f3733
Charlie <104016616+Chbe19@users.noreply.github.com>
2025-03-10 15:28:51 +0100

    api-key

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from Services import *
 from Functions import *
-
+from openai import OpenAI
+import config
 
 def main():
-    print("Hello, World!")
+    client = OpenAI(api_key= config.OPENAI_API_KEY)
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        store=True,
+        messages=[
+            {"role": "user", "content": "write a haiku about ai"}
+        ]
+    )
+
+    print(completion.choices[0].message)
 
 if __name__ == "__main__":
     main()
